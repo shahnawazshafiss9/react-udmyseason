21bc1b4a9b5319bbe902a37f8876515e1fc4c41b
Shahnawaz Alam <shahnawazshafiss9@gmail.com>
2021-02-01 18:10:56 +0530

    coseason working

--- src/SeasonDisplay.js
+++ src/SeasonDisplay.js
@@ -1,5 +1,16 @@
 import React from 'react'
+import './seasonDisplay.css'
+const sesionConfig = {
+    Winter : {
+        text: 'Burr, it is chilly',
+        iconName: 'snowflake'
 
+    },
+    Summer : {
+        text: 'Lets hit the beach',
+        iconName: 'sun'
+    }
+}
 const getSeasion = (lat, month) => {
     if(month > 2 && month < 9){
         return lat > 0 ? 'Summer' : 'Winter';
@@ -10,9 +21,18 @@ const getSeasion = (lat, month) => {
 }
 const SeasonDisplay = (props) => {
     const seasion = getSeasion(props.lat, new Date().getMonth());
-    const text = seasion === 'Winter'? 'Burr, it is chilly' : 'Lets hit the beach';
+const {text, iconName} = sesionConfig[seasion];
+
+
+    // const text = seasion === 'Winter'? 'Burr, it is chilly' : 'Lets hit the beach';
+    // const icon = seasion === 'winter' ? 'snowflake' : 'sun'
+
     return (
-        <div>{text}</div>
+        <div className={`season-display ${seasion}`}>
+            <i className={ ` icon-left massive ${iconName} icon`} />
+            <h1>{text}</h1>
+            <i className={ ` icon-right massive ${iconName} icon`} />
+        </div>
     );
 }
 
