19758ac4e4ed7129c34dd0f2599a73600fb0cbf5
Shahnawaz Alam <shahnawazshafiss9@gmail.com>
2021-02-02 16:18:51 +0530

    section 6 done

--- src/index.js
+++ src/index.js
@@ -1,37 +1,45 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import SeasonDisplay from './SeasonDisplay'
-class App extends React.Component{
+import Spinner from './Spinner'
+class App extends React.Component {
     // constructor(props){
     //     super(props)
     //     this.state = {
     //         lat: null,
     //         errorMessage: ''
     //     }
-        
+
 
     // }
     state = {
         lat: null,
         errorMessage: ''
     }
-    componentDidMount(){
+    componentDidMount() {
         window.navigator.geolocation.getCurrentPosition(
-            position => this.setState({lat: position.coords.latitude}),
-            err => this.setState({errorMessage: err.message})
+            position => this.setState({ lat: position.coords.latitude }),
+            err => this.setState({ errorMessage: err.message })
         )
     }
     // componentDidUpdate(){
     //     console.log('My docuemtn was just updated - it rendered');
     // }
-    render(){
-        if(this.state.errorMessage && !this.state.lat){
+    renderContent() {
+        if (this.state.errorMessage && !this.state.lat) {
             return <div>Error: {this.state.errorMessage}</div>;
         }
-        if(!this.state.errorMessage && this.state.lat){
+        if (!this.state.errorMessage && this.state.lat) {
             return <SeasonDisplay lat={this.state.lat} />;
         }
-        return <div>Loading!!!</div>;
+        return <Spinner message="Please select location" />;
+    }
+    render() {
+        return (
+            <div className="border red">
+                {this.renderContent()}
+            </div>
+        );
     }
 }
 ReactDOM.render(
